d4b9a4ba56034ff8ff3af6d970339d4eb0f6e760
werner_m <mirko.werner@valiton.com>
2025-06-17 16:00:42 +0200

    fix enable_external_secrets_stackit_secrets_manager variable

--- stackit/bootstrapping/gitops-ske-addons/variables.tf
+++ stackit/bootstrapping/gitops-ske-addons/variables.tf
@@ -39,11 +39,11 @@ variable "addons" {
   description = "Kubernetes addons"
   type        = any
   default = {
-    enable_ingress_nginx                                 = true
-    enable_cert_manager                                  = true
-    enable_external_secrets                              = true
-    enable_external_secrets_with_stackit_secrets_manager = true
-    enable_kube_prometheus_stack                         = true
+    enable_ingress_nginx                            = true
+    enable_cert_manager                             = true
+    enable_external_secrets                         = true
+    enable_external_secrets_stackit_secrets_manager = true
+    enable_kube_prometheus_stack                    = true
   }
 
 }
