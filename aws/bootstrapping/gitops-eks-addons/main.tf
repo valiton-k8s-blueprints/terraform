084b6afa4528805ebb1329e8d6dc5d1b94d12b8c
Chien-Hao Chiu <chienhao.chiu@valiton.com>
2025-06-18 14:24:43 +0200

    add enable_kube_prometheus_stack variable

--- aws/bootstrapping/gitops-eks-addons/main.tf
+++ aws/bootstrapping/gitops-eks-addons/main.tf
@@ -24,6 +24,7 @@ locals {
   gitops_argocd_chart_version = var.gitops_argocd_chart_version
 
   kube_prometheus_stack_namespace = try(var.kube_prometheus_stack.namespace, "kube-prometheus-stack")
+  enable_prometheus_federation    = try(var.kube_prometheus_stack.enable_prometheus_federation, false)
 
   aws_addons = {
     enable_aws_efs_csi_driver           = try(var.addons.enable_aws_efs_csi_driver, false)
@@ -71,7 +72,10 @@ locals {
       applications_repo_path     = local.gitops_applications_repo_path
       applications_repo_revision = local.gitops_applications_repo_revision
     },
-    { kube_prometheus_stack_namespace = local.kube_prometheus_stack_namespace },
+    {
+      kube_prometheus_stack_namespace = local.kube_prometheus_stack_namespace
+      enable_prometheus_federation    = local.enable_prometheus_federation
+    },
     { cloud_provider = "aws" },
     var.custom_gitops_metadata
   )
