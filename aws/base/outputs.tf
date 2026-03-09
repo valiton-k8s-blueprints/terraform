5210e74d3e65caee7b33140a84b96730c66a46b1
mcpomm <prima34@gmx.net>
2026-03-09 19:32:42 +0100

    Update outputs.tf

--- aws/base/outputs.tf
+++ aws/base/outputs.tf
@@ -28,6 +28,11 @@ output "vpc_cidr_block" {
   value       = module.vpc.vpc_cidr_block
 }
 
+output "nat_public_ips" {
+  description = "List of public Elastic IPs created for AWS NAT Gateway"
+  value       = module.vpc.nat_public_ips
+}
+
 output "vpc_id" {
   description = "The ID of the VPC"
   value       = module.vpc.vpc_id
