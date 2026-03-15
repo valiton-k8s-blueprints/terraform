13b8acefdff239ce391e5d0e0fd19b6361e7a7ae
mcpomm <prima34@gmx.net>
2026-03-15 17:21:31 +0100

    Update outputs.tf

--- aws/base/outputs.tf
+++ aws/base/outputs.tf
@@ -64,6 +64,11 @@ output "cluster_security_group_id" {
   value       = module.eks.cluster_security_group_id
 }
 
+output "node_security_group_id" {
+  description = "ID of the node shared security group."
+  value       = module.eks.node_security_group_id
+}
+
 output "cluster_certificate_authority_data" {
   description = "Base64 encoded certificate data required to communicate with the cluster."
   value       = module.eks.cluster_certificate_authority_data
