4ac55e7480b3724051ee0a02e2d5df4314ffdde3
Uwe Jäger <uwe.jaeger@valiton.com>
2026-05-13 17:30:48 +0200

    Increase LB listener timeout

--- bws/base/modules/bastion/outputs.tf
+++ bws/base/modules/bastion/outputs.tf
@@ -4,6 +4,7 @@ output "instance" {
 }
 
 output "public_ip" {
-  value = openstack_networking_floatingip_v2.public_ip.address
+#  value = openstack_networking_floatingip_v2.public_ip.address
+  value = "127.0.0.1" # dummy
 }
 
